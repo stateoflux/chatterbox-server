6f2cf99b3f3bdb4784cb8201e8c2487b64fcef46
Hack Reactor Students <wayne.montague@gmail.com>
2013-12-07 12:04:15 -0800

    Removed console log statements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -25,7 +25,7 @@ var makeController = function() {
     getMessages: function() {
       this.promise = $.ajax({
         // always use this url
-        url: url + "?order=-createdAt" + "&limit=20",
+        url: url,    // + "?order=-createdAt" + "&limit=20",
         type: 'GET',
         contentType: 'application/json',
         success: function (data) {
@@ -50,7 +50,7 @@ var makeController = function() {
         // debugger;
         for (var i = messages.length - 1; i > messages.length - 20; i--) {
           // debugger;
-          console.log("renderMessages: " + that.currentRoom);
+          // console.log("renderMessages: " + that.currentRoom);
           // debugger;
           var msgHTML=  messages[i].renderMessage(that.currentRoom);
           $chatSession = $('.chat-session');
