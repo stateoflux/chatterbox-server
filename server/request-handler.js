c888baae29cd5baaa6b87b0f43846967da01a65f
Wayne Montague <wayne.montague@gmail.com>
2013-12-08 12:21:35 -0800

    created several helper functions to modularize code.

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,54 +5,54 @@ var headers = {
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
   "access-control-max-age": 10, // Seconds.
-  "Content-Type": "application/json"
+  'Content-Type': "application/json"
 };
 
 var dummyMessage = {
   'username': 'batman',  // gets username from url
-  'text': 'Pow', 
+  'text': 'Pow',
   'roomname': 'lobby'
 };
-
-var statusCode = 200; // default status code
-var _messages = [dummyMessage];
-var responseBody;
+var messages = [dummyMessage];
 
 var roomnameFilter = function(roomToFilter) {
-  return _.where(_messages, {roomname: roomToFilter});
+  return _.where(messages, {roomname: roomToFilter});
 };
 
-var returnMessages = function(request) {
+var sendResponse = function(response, obj, status) {
+  status = status || 200;
+  response.writeHead(status, headers);
+  response.end(JSON.stringify(obj));
+};
+
+var returnMessages = function(request, response) {
   console.log("requesting messages");
   var paths = request.url.slice(1).split('/');
   console.log(paths);
   if (paths[0] === 'classes' && paths[1] === 'messages') {
-    console.log(responseBody);
-    responseBody = { results: _messages };
+    sendResponse(response, { results: messages });
   } else if(paths[0] === 'classes' && paths[1] !=='messages') {
-    responseBody = { results: roomnameFilter(paths[1]) };  // function to filter messages
+    sendResponse(response, { results: roomnameFilter(paths[1]) });
   } else {
-    statusCode = 404;
+    sendResponse(response, null, 404);
   }
 };
 
 // Save received chat messages into storage
-var saveMessages = function(request){
-  statusCode = 201;
+var saveMessages = function(request, response){
+  var partialMessage = "";
   request.setEncoding('utf8');
   request.on('data', function(chunk) {
-    _messages.push(JSON.parse(chunk));
+    partialMessage += chunk;
+  });
+  request.on('end', function() {
+    messages.push(partialMessage);
+    sendResponse(response, "", status);
   });
 };
 
 var respondToOptions = function(request, response) {
-  debugger;
-  responseBody = '';
-  request.on('end', function() {
-    console.log("about to send response");
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(responseBody));
-  });
+  sendResponse(response, null);
 };
 
 var actionList = {
@@ -64,14 +64,7 @@ var actionList = {
 // Request Handler
 // ============================================================================
 exports.handleRequest = function(request, response) {
-
   console.log("Serving request type " + request.method + " for url " + request.url);
-
   actionList[request.method](request, response);
-  // debugger;
-  // request.on('end', function() {
-  //   console.log("about to send response");
-  //   response.writeHead(statusCode, headers);
-  //   response.end(JSON.stringify(responseBody));
-  // });
 };
+
