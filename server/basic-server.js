a52436730817f828ee6b44e97d553c5e5d377700
Wayne Montague <wayne.montague@gmail.com>
2013-12-08 19:38:07 -0800

    refactored server code by introducing a router object.

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,8 +1,29 @@
 var http = require("http");
+var url = require("url");
 var requestHandler = require("./request-handler");
 var port = 8081;
 var ip = "127.0.0.1";
-var server = http.createServer(requestHandler.handleRequest);
+
+var messageHandler = requestHandler.handleRequest;
+var rootHandler = function() {
+  console.log("will serve index.html soon")
+}
+
+var router = {
+  "/": rootHandler,
+  "/classes/messages": messageHandler
+};
+var server = http.createServer(function(request, response) {
+  var path = url.parse(request.url).pathname;
+  var handler = router[path];
+
+  if (handler) {
+    handler(request, response);
+  }
+  else {
+
+  }
+});
 
 console.log("Listening on http://" + ip + ":" + port);
 
